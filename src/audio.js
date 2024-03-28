513c79f26678d617484135affcd74e7742eaa36f
Venedykt Mitishov <chewbaccayeti@gmail.com>
2024-03-28 17:59:49 +0200

    Wavesurfer.js player

--- src/audio.js
+++ src/audio.js
@@ -1,9 +1,41 @@
-const music = document.getElementById("background-music");
-
-function playMusic() {
-    music.play().catch(function (error) {
-        console.error("Ошибка при воспроизведении музыки: ", error);
+document.addEventListener('DOMContentLoaded', function () {
+    const wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'lightblue',
+        progressColor: 'darkblue'
     });
-}
 
-window.onload = playMusic;
+    wavesurfer.load('../audio/Unprocessed_-_Deadrose_(Vuxo7.com).mp3');
+
+    wavesurfer.on('ready', function () {
+        console.log('Аудіофайл завантажен та готов к відтворення');
+        wavesurfer.play();
+    });
+
+    wavesurfer.on('ready', function () {
+        document.getElementById('duration').textContent = formatTime(wavesurfer.getDuration());
+        wavesurfer.play();
+    });
+
+    wavesurfer.on('audioprocess', function () {
+        document.getElementById('current-time').textContent = formatTime(wavesurfer.getCurrentTime());
+    });
+
+    function formatTime(time) {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+
+
+    document.getElementById('playPause').addEventListener('click', function () {
+        wavesurfer.playPause();
+    });
+
+    wavesurfer.on('play', function () {
+        console.log('Відтворення');
+    });
+    wavesurfer.on('pause', function () {
+        console.log('Пауза');
+    });
+});
